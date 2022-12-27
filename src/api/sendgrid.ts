d823be1561020a0d9f1b91776aeb64ab72d8d302
Will <will@wdew.uk>
2022-12-27 06:26:10 +0000

    Making logic more D.R.Y

--- src/api/sendgrid.ts
+++ src/api/sendgrid.ts
@@ -31,6 +31,13 @@ router.post('/send-mail', (req, res) => {
     message: 'Template not found'
   })
 
+  let email = {
+    to,
+    from,
+    subject,
+    text: message,
+    html: `<strong>${message}</strong>`,
+  };
   if (useTemplate) {
     const {
       fileName,
@@ -42,37 +49,16 @@ router.post('/send-mail', (req, res) => {
     const template = Handlebars.compile(fileContent);
     const content = template({ ...replacements, ...templateReplacements });
 
-    sgMail
-    .send({
-      to,
-      from,
+    email = {
+      ...email,
       subject: templateSubject ? templateSubject : subject,
       text: content,
       html: content,
-    })
-    .then(() => {
-      res.json({
-        message: "[SEND GRID]: Email Sent."
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "[SEND GRID]: Email Send Failed.",
-        error,
-      });
-      console.error(error);
-    });
-    return;
+    };
   }
 
   sgMail
-    .send({
-      to,
-      from,
-      subject,
-      text: message,
-      html: `<strong>${message}</strong>`,
-    })
+    .send(email)
     .then(() => {
       res.json({
         message: "[SEND GRID]: Email Sent."
@@ -85,7 +71,6 @@ router.post('/send-mail', (req, res) => {
       });
       console.error(error);
     });
-
 });
 
 export default router;
